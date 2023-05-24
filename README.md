24446a94248f64e2fc986defeaef40761a20277f
Zackzep <103279212+Zackzep@users.noreply.github.com>
2023-05-23 19:49:44 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# L10_Into_To_Java_Pt2
+ 
---
+++ src/NewDisplay.java
@@ -0,0 +1,7 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class NewDisplay {
+    public static void main(String[] args) {
+        System.out.println("Running from new display");
+    }
+}
